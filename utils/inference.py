8c729a4a710853f6256963a9ba79998bd125e7e2
sayedgamal99 <sayyedgamall@gmail.com>
2025-03-14 07:13:10 +0200

    simple inference script

---
+++ utils/inference.py
@@ -0,0 +1,13 @@
+from face_recognition import FaceRecognition
+
+# Initialize face recognition pipeline
+face_recognition = FaceRecognition()
+
+
+# Test the pipeline on special images
+results = face_recognition.test_on_images()
+# print(results)
+
+
+# Generate submissions using all models
+# all_submissions = face_recognition.run_all_models()
